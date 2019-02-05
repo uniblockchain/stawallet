190c09de66aab399c9b9684da81759a4c6dcf61c
Mahdi Aali <mahdi13.1373@gmail.com>
2019-02-05 12:02:00 +0300

    Add some document to each bitcoin block watcher methods

--- src/main/kotlin/stacrypt/stawallet/bitcoin/BitcoinDaemon.kt
+++ src/main/kotlin/stacrypt/stawallet/bitcoin/BitcoinDaemon.kt
@@ -56,6 +56,9 @@ object bitcoind : WalletDaemon() {
         }
     }
 
+    /**
+     * Here we are serching for any associated address in our watching addresses for each incoming transaction utxo.
+     */
     private fun findAssociatedAddress(vout: TransactionOutput) = transaction {
         AddressTable
             .select {
@@ -63,10 +66,26 @@ object bitcoind : WalletDaemon() {
             }.lastOrNull()?.run { AddressDao.wrapRow(this) }
     }
 
-    
+    /**
+     * Here we add a new confirmaton event whenever a new block found.
+     *
+     * Since the transaction fully confirmed, regardless of the previous event of the transaction, we'll insert a new
+     * confirmation event for the transaction and related invoice.
+     */
+    private fun addNewConfirmationEvent(
+        address: AddressDao,
+        transaction: Transaction,
+        transactionOutput: TransactionOutput
+    ) {
+
+    }
 
     /**
-     * Here we make a new utxo record, because we are SURE that it is a fully confirmed utxo
+     * Here we make a new utxo record, because we are SURE that it is a fully confirmed utxo.
+     *
+     * Note: We'll make a UTXO record per incoming transaction inputs, and the index of it. Regardless of the
+     * address of the sender. For example if we have 2 input from a single sender, we will generate 2 separated UTXOs.
+     *
      */
     private fun insertNewUtxo(address: AddressDao, transaction: Transaction, transactionOutput: TransactionOutput) =
         transaction {
@@ -85,26 +104,32 @@ object bitcoind : WalletDaemon() {
 
     /**
      * Here we make a new deposit record, because we are SURE that it is a fully confirmed transaction.
+     *
+     * Note: The saved deposits are belongs to a specific invoice. If we couldn't find any related invoice, we will not
+     * record this deposit.
+     *
+     * Note 2: Before calling this method, your data should be grouped by invoices. For example if we have 2 input from
+     * a single sender, we will not save it. You should sum all amounts of a specific invoice in a specific tx, then
+     * call this method. It means we have a unique constraint by transaction hash and invoice.
+     *
      */
     private fun insertNewDeposit(address: AddressDao, transaction: Transaction, transactionOutput: TransactionOutput) =
         transaction {
-            transaction {
-                // Find the related invoice
-                val relatedInvoice = InvoiceTable
-                    .select { InvoiceTable.wallet eq address.wallet.id }
-                    .andWhere { InvoiceTable.address eq address.id }
-                    .andWhere { InvoiceTable.expiration.isNull() or (InvoiceTable.expiration greater DateTime.now()) }
-                    .lastOrNull()?.run { InvoiceDao.wrapRow(this) }
+            // Find the related invoice
+            val relatedInvoice = InvoiceTable
+                .select { InvoiceTable.wallet eq address.wallet.id }
+                .andWhere { InvoiceTable.address eq address.id }
+                .andWhere { InvoiceTable.expiration.isNull() or (InvoiceTable.expiration greater DateTime.now()) }
+                .lastOrNull()?.run { InvoiceDao.wrapRow(this) }
 
-                if (DepositTable.select { DepositTable.invoice eq relatedInvoice!!.id }.count() == 0) {
-                    // This is new UTXO!
-                    UtxoDao.new {
-                        this.address = address
-                        this.wallet = address.wallet
-                        this.txid = transaction.hash!!
-                        this.vout = transactionOutput.n!!.toInt()
-                        this.amount = transactionOutput.value!!.toLong()
-                    }
+            if (DepositTable.select { DepositTable.invoice eq relatedInvoice!!.id }.count() == 0) {
+                // This is new UTXO!
+                UtxoDao.new {
+                    this.address = address
+                    this.wallet = address.wallet
+                    this.txid = transaction.hash!!
+                    this.vout = transactionOutput.n!!.toInt()
+                    this.amount = transactionOutput.value!!.toLong()
                 }
             }
         }
