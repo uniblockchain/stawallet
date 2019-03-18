924a2e615d0f1acf144d5025d07f5437d84924e5
Mahdi Aali <mahdi13.1373@gmail.com>
2019-03-18 13:17:03 +0300

    Fix docker problems

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,38 +1,13 @@
 #!/bin/bash
-help () {
-	echo "Available daemons are: "
-	echo "  api"
-	echo "  watcher [wallet-name]"
-	echo "  cli"
-}
 
-echo "@=$@"
-
-if [ $# -lt 1 ];then
-	help
-	exit 0
-fi
-
-SERVICE=$1
-
-#cd ~/stawallet
+set -e
 
 ./bin/stawallet database init
 ./bin/stawallet database populate
 ./bin/stawallet database migrate
 
-case $SERVICE in 
-	api)
-		./bin/stawallet serve
-		;;
-	btc_watcher)
-		./bin/stawallet watch $2
-		;;
-	cli)
-        alias stawallet=./bin/stawallet
-		tail -f /dev/null
-		;;
-  *)
-		exec $@
-		;;
-esac
+if [ "$1" = 'stawallet' ]; then
+    exec ./bin/stawallet "$@"
+fi
+
+exec "$@"
