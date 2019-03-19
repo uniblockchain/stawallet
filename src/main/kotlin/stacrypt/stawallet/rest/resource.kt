6defefd659515cd274db999e3486d920af0afc0a
Mahdi Aali <mahdi13.1373@gmail.com>
2019-03-19 13:13:13 +0300

    Fix datetime seralization format

--- src/main/kotlin/stacrypt/stawallet/rest/resource.kt
+++ src/main/kotlin/stacrypt/stawallet/rest/resource.kt
@@ -195,3 +195,32 @@ data class WithdrawResource(
     }
 
 }
+
+/**
+ * RegExp to test a string for a ISO 8601 Date spec
+ *  YYYY
+ *  YYYY-MM
+ *  YYYY-MM-DD
+ *  YYYY-MM-DDThh:mmTZD
+ *  YYYY-MM-DDThh:mm:ssTZD
+ *  YYYY-MM-DDThh:mm:ss.sTZD
+ * @see: https://www.w3.org/TR/NOTE-datetime
+ * @type {RegExp}
+ */
+var ISO_8601 = "^\\d{4}(-\\d\\d(-\\d\\d(T\\d\\d:\\d\\d(:\\d\\d)?(\\.\\d+)?(([+-]\\d\\d:\\d\\d)|Z)?)?)?)?\$".toRegex()
+
+
+/**
+ * RegExp to test a string for a full ISO 8601 Date
+ * Does not do any sort of date validation, only checks if the string is according to the ISO 8601 spec.
+ *  YYYY-MM-DDThh:mm:ss
+ *  YYYY-MM-DDThh:mm:ssTZD
+ *  YYYY-MM-DDThh:mm:ss.sTZD
+ * @see: https://www.w3.org/TR/NOTE-datetime
+ * @type {RegExp}
+ */
+var ISO_8601_FULL = "^\\d{4}-\\d\\d-\\d\\dT\\d\\d:\\d\\d:\\d\\d(\\.\\d+)?(([+-]\\d\\d:\\d\\d)|Z)?\$".toRegex()
+
+fun String.isUtcDate() = ISO_8601.matches(this)
+fun String.isFullUtcDate() = ISO_8601_FULL.matches(this)
+
